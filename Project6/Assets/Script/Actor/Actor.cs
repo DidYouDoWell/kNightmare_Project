2615dfb6c0b4929335d3f739fcc1a967d78ef5b4
foo2002 <foo1992@naver.com>
2017-06-20 17:43:22 +0900

    Base
    q123

---
+++ Project6/Assets/Script/Actor/Actor.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : BaseObject
+{
+    bool IsPlayer = false;
+    public bool IS_PLAYER
+    {
+        get { return IsPlayer; }
+        set { IsPlayer = value; }
+    }
+
+	[SerializeField]
+	eTeamType TeamType;
+	public eTeamType TEAM_TYPE
+	{
+		get
+		{
+			return TeamType ;
+		}
+	}
+
+	[SerializeField]
+	string TemplateKey = string.Empty;
+
+	GameCharacter SelfCharacter = null;
+	public GameCharacter SELF_CHARACTER
+	{ get { return SelfCharacter; } }
+
+
+	// AI
+	//BaseAI ai = null;
+	//public BaseAI AI
+	//{ get { return ai; } }
+
+
+	BaseObject TargetObject = null;
+
+	// Board -> HP Bar
+	[SerializeField]
+	bool bEnableBoard = true;
+
+	private void Awake()
+	{
+        //건희
+		//GameObject aiObject = new GameObject();
+		//aiObject.name = "NormalAI";
+		//ai = aiObject.AddComponent<NormalAI>(); // AI가 많아지면 해당 항목을 스위치문으로 세분화해서 작성가능함.
+		//aiObject.transform.SetParent(SelfTransform);// AI타겟을 자신으로 설정함.
+
+		//// 없으면 동작  X
+		//ai.Target = this;
+
+		GameCharacter gameCharacter =
+			CharacterManager.Instance.AddCharacter(TemplateKey);
+		gameCharacter.TargetComponenet = this;
+		SelfCharacter = gameCharacter;
+
+		for(int i = 0; 
+			i< gameCharacter.CHARACTER_TEMPLATE.LIST_SKILL.Count;
+			i++)
+		{
+			SkillData data =
+				SkillManager.Instance.GetSkillData(
+				gameCharacter.CHARACTER_TEMPLATE.LIST_SKILL[i]);
+
+			if (data == null)
+			{
+				Debug.LogError(
+					gameCharacter.CHARACTER_TEMPLATE.LIST_SKILL[i]
+					+ " 스킬 키를 찾을수 없습니다.");
+			}
+			else
+				gameCharacter.AddSkill(data);
+		}
+        //재인이형
+		//if(bEnableBoard)//UI(boar)
+		//{
+		//	BaseBoard board = BoardManager.Instance.AddBoard(
+		//		this, eBoardType.BOARD_HP);
+
+		//	board.SetData(ConstValue.SetData_HP,
+		//		GetStatusData(eStatusData.MAX_HP),
+		//		SelfCharacter.CURRENT_HP);
+		//}
+
+		ActorManager.Instance.AddActor(this);//Actor가 생성된 이후에 처리
+	}
+
+	public double GetStatusData(eStatusData statusData)
+	{
+		return SelfCharacter.CHARACTER_STATUS.GetStatusData(statusData);
+	}
+    //대문자 오브젝트 클래스의 상위객체
+    //소문자 오브젝트 자료형태(int형 float형 등등....)
+
+    //파람즈 - 매개변수들(갯수제한, 형태제한 없음.) 불안전함. 최대한 간단한 데이터만. 규칙을 정리해야함.
+    public override object GetData(string keyData, params object[] datas)
+	{
+		if (keyData == ConstValue.ActorData_Team)
+			return TeamType;
+		else if (keyData == ConstValue.ActorData_Character)
+			return SelfCharacter;
+		else if (keyData == ConstValue.ActorData_GetTarget)
+			return TargetObject;
+		else if(keyData == ConstValue.ActorData_SkillData)
+		{
+			int index = (int)datas[0];
+			return SelfCharacter.GetSkillByIndex(index);
+		}
+
+
+		// Base 부모클래스 -> BaseObject
+		return base.GetData(keyData, datas);
+	}
+    //메소드가 사용되어야 하는 상황을 구분짓기 위해 두개를 나눔.
+	public override void ThrowEvent(string keyData, params object[] datas)
+	{
+		if(keyData == ConstValue.EventKey_Hit)
+		{
+			if (OBJECT_STATE == eBaseObjectState.STATE_DIE)
+				return;
+
+			// 공격 주체의 케릭터
+			GameCharacter casterCharacter
+				= datas[0] as GameCharacter;
+			SkillTemplate skillTemplate =
+				datas[1] as SkillTemplate;
+
+			casterCharacter.CHARACTER_STATUS.AddStatusData("SKILL",
+				skillTemplate.STATUS_DATA);
+            //데미지 수치를 랜덤값으로 만들면 더 좋다. 규칙을 정한것이지 구조가 아님. 다른곳에서도 처리 가능함.
+			double attackDamage =
+				casterCharacter
+				.CHARACTER_STATUS
+				.GetStatusData(eStatusData.ATTACK);
+			// SelfCharacter.CHARACTER_STATUS.GetStatusData(eStatusData.DEFFENCE);
+
+			casterCharacter.CHARACTER_STATUS.RemoveStatusData("SKILL");
+
+			SelfCharacter.IncreaseCurrentHP(-attackDamage);
+
+            //재인이형
+			//// HPBoard
+			//BaseBoard board = BoardManager.Instance.GetBoardData(this,
+			//	eBoardType.BOARD_HP);
+			//if (board != null)
+			//	board.SetData(ConstValue.SetData_HP,
+			//		GetStatusData(eStatusData.MAX_HP),
+			//		SelfCharacter.CURRENT_HP);
+
+            //재인이형
+			//// Board 초기화
+			//board = null;
+
+			//// DamageBoard 
+			//board = BoardManager.Instance.AddBoard(this, eBoardType.BOARD_DAMAGE);
+			//if (board != null)
+			//	board.SetData(ConstValue.SetData_Damage, attackDamage);
+
+            //건희
+			//// 피격 에니메이션
+			//AI.ANIMATOR.SetInteger("Hit", 1);
+		}
+		else if(keyData == ConstValue.EventKey_SelectSkill)
+		{
+			int index = (int)datas[0];
+			SkillData data = SelfCharacter.GetSkillByIndex(index);
+			SelfCharacter.SELECT_SKILL = data;
+		}
+		else if(keyData == ConstValue.ActorData_SetTarget)
+		{
+			TargetObject = datas[0] as BaseObject;
+		}
+		else
+			base.ThrowEvent(keyData, datas);
+	}
+
+	protected virtual void Update()
+	{
+        //건희
+		//AI.UpdateAI();
+		//if(AI.END)
+		//{
+		//	Destroy(SelfObject);
+		//}
+	}
+    //스킬 템플릿이라는 퍼즐조각이 있고 그걸 조합해서 만든게 스킬 데이터
+	public void RunSkill()
+	{
+		SkillData selectSkill = SelfCharacter.SELECT_SKILL;
+		if (selectSkill == null)
+			return;
+        //코루틴을 넣거나 딜레이를 잘 섞어서 스킬을 만듬.
+        // 은기
+		//for(int i = 0; i < selectSkill.SKILL_LIST.Count;i++)
+		//{
+		//	SkillManager.Instance.RunSkill(this, 
+		//		selectSkill.SKILL_LIST[i]);
+		//}
+	}
+
+	private void OnEnable()
+	{
+        // 재인이형
+		//if (BoardManager.Instance != null)
+		//	BoardManager.Instance.ShowBoard(this, true);
+	}
+
+	public void OnDisable()
+	{
+        // 재인이형
+        //if (BoardManager.Instance != null
+        //          && GameManager.Instance.GAME_OVER == false)
+        //	BoardManager.Instance.ShowBoard(this, false);
+    }
+    //플레이어를 찾아보고 있을때 클리어함.
+    public void OnDestroy()
+	{
+        // 재인이형
+        //if (BoardManager.Instance != null)
+        //	BoardManager.Instance.ClearBoard(this);
+
+        if (ActorManager.Instance != null)
+		{
+			ActorManager.Instance.RemoveActor(this);
+		}
+	}
+
+}
---
+++ Project6/Assets/Script/Actor/Player/Player.cs
@@ -0,0 +1,223 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerController))]
+
+public class Player : MonoBehaviour {
+
+
+	protected ePlayerStateType CurrentState = ePlayerStateType.STATE_IDLE;
+	public ePlayerStateType CURRENT_STATE
+	{
+		get { return CurrentState; }
+	}
+	Animator Anim = null;
+	public Animator ANIMATOR
+	{
+		get
+		{
+			if (Anim == null)
+			{
+				Anim = gameObject.GetComponentInChildren<Animator>();
+			}
+			return Anim;
+		}
+	}
+
+	bool bAttack = false;
+	public bool IS_ATTACK
+	{
+		get { return bAttack; }
+		set { bAttack = value; }
+	}
+
+
+	/// <summary>
+	/// /
+	/// </summary>
+
+
+
+	//Vector3 vec3Dis;
+	Vector3 playerposition;
+	Vector3 preTransform;
+	float curTime=0f;
+	bool playerOnClicked = false;
+
+	//public float MoveSpeed;
+	public float Dis = 0;
+	public float limitDis = 8;
+	public float moveSpeed = 5;
+	PlayerController controller;
+	Camera viewCamera;
+	Vector3 heightCorrectedPoint;
+	bool Moving=false;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<PlayerController>();
+		viewCamera = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+
+		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move(moveVelocity);
+
+		Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		float rayDistance;
+
+
+
+		if (groundPlane.Raycast(ray, out rayDistance))
+		//true라면 ray가 바닥 플레인과 교차한 것이고 
+		//그러면 카메라에서 ray가 부딫친 지점까지의 거리를 알 수 있고,
+		//그럼 실제 교차 지점을 지정할 수 있음.
+		{
+
+			Vector3 point = ray.GetPoint(rayDistance);
+			//Debug.Log(rayDistance);
+			Debug.DrawLine(ray.origin, point, Color.red);
+
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, rayDistance))
+			{
+				if (hit.collider.tag=="Player")
+				{
+					playerOnClicked = true;
+
+					
+
+				}
+			}
+
+				//
+				if (Input.GetMouseButton(0) && Moving == false && playerOnClicked==true)
+			{
+
+				//
+				ProcessCrouch();
+
+				heightCorrectedPoint = new Vector3(point.x, transform.position.y, point.z);
+
+
+				//	vec3Dis = heightCorrectedPoint - transform.position;
+				//con = vec3Dis.normalized * moveSpeed;
+
+
+				Debug.Log(heightCorrectedPoint);
+				controller.LookAt(heightCorrectedPoint);
+
+				preTransform = transform.position;
+				playerposition = preTransform;
+				//Dis = Vector3.Distance(preTransform, heightCorrectedPoint);
+
+			}
+
+
+
+
+		}
+
+
+		if ((Input.GetMouseButtonUp(0) || Moving == true) &&playerOnClicked==true)
+		{
+
+			ProcessAttack();
+
+			Moving = true;
+			curTime += Time.deltaTime;
+
+			//움직이는 속도를 일정하게 할 것인가?
+			playerposition= Vector3.MoveTowards(preTransform, heightCorrectedPoint, limitDis* curTime* 3f);
+			//playerposition = Vector3.Lerp(playerposition, heightCorrectedPoint, curTime*MoveSpeed );
+
+			transform.position=playerposition;
+
+			Dis = Vector3.Distance(preTransform, playerposition);
+
+			if (transform.position == heightCorrectedPoint)
+			{
+				Moving = false;
+				curTime = 0;
+				playerOnClicked = false;
+				
+			}
+
+			if(Dis >= limitDis)
+			{
+				Moving = false;
+				curTime = 0;
+				playerOnClicked = false;
+				//ProcessIdle();
+			}
+		}
+		//if (Input.GetMouseButtonUp(0)|| Moving==true)
+		//{
+		//	Moving = true;
+		//	Debug.Log("Move!");
+
+		//	Vector3 ContmoveVelocity = heightCorrectedPoint.normalized * moveSpeed;
+		//	controller.Move(ContmoveVelocity);
+
+		//	if(controller.transform.position== heightCorrectedPoint)
+		//	{
+		//		Moving = false;
+		//	}
+		//}
+		
+
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag=="Enemy")
+		{
+			Destroy(collision.gameObject);
+		}
+	}
+
+
+	void ChangeAnimation()
+	{
+		if (ANIMATOR == null)
+		{
+			Debug.LogError(gameObject.name + " 에게 Animator가 없습니다.");
+			return;
+		}
+
+		ANIMATOR.SetInteger("State", (int)CurrentState);
+	}
+
+	protected virtual void ProcessIdle()
+	{
+		CurrentState = ePlayerStateType.STATE_IDLE;
+		ChangeAnimation();
+	}
+
+	protected virtual void ProcessCrouch()
+	{
+		CurrentState = ePlayerStateType.STATE_CROUCH;
+		ChangeAnimation();
+	}
+
+	protected virtual void ProcessAttack()
+	{
+		//Target.ThrowEvent(ConstValue.EventKey_SelectSkill, 0);
+		CurrentState = ePlayerStateType.STATE_ATTACK;
+		ChangeAnimation();
+	}
+
+	protected virtual void ProcessDie()
+	{
+		CurrentState = ePlayerStateType.STATE_DEAD;
+		ChangeAnimation();
+	}
+
+
+}
