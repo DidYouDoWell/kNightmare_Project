3fb9acd17b6fde2e6e3703e71edb3b35486cf9c0
DidYouDoWell <alien5979@naver.com>
2017-06-21 13:36:18 +0900

    test
    dd

---
+++ Project6/Assets/Script/AI/NonPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonPlayer : Actor {
+
+
+	public BaseAI ai = null;
+	public BaseAI AI
+	{ get { return ai; } }
+
+	bool IsInvincibility = false;
+	public bool INVINCIBILITY
+	{
+		get { return IsInvincibility; }
+		set { IsInvincibility = value; }
+	}
+}
