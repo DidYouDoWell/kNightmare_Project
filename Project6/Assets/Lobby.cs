8ae2cf471a0c5379c93b71ad177482af6c31280a
JayLee2017 <dlwodls10@navercom>
2017-06-21 11:11:41 +0900

    UIBase Added

---
+++ Project6/Assets/Lobby.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lobby : MonoBehaviour
+{
+	UIButton NewGameBtn;
+	UIButton ContinueBtn;
+	UIButton TitleBtn;
+
+	void Start ()
+	{
+		NewGameBtn = GameObject.Find("NewGameButton").GetComponent<UIButton>();
+		ContinueBtn = GameObject.Find("ContinueButton").GetComponent<UIButton>();
+		TitleBtn = GameObject.Find("TitleButton").GetComponent<UIButton>();
+
+		EventDelegate.Add(NewGameBtn.onClick, () =>
+		{
+			print("NewGameButton is OK.");
+		}
+			);
+
+		EventDelegate.Add(ContinueBtn.onClick, () =>
+		{
+			print("ContinueButton is OK.");
+		}
+			);
+
+		EventDelegate.Add(TitleBtn.onClick, () =>
+		{
+			print("TitleButton is OK.");
+		}
+			);
+	}
+
+	void Update ()
+	{
+		
+	}
+}
---
+++ Project6/Assets/Script/UI/Title.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+	UIButton StartBtn;
+	UIButton ExitBtn;
+	
+	void Start ()
+	{
+		StartBtn = GameObject.Find("StartButton").GetComponent<UIButton>();
+		ExitBtn = GameObject.Find("ExitButton").GetComponent<UIButton>();
+
+		EventDelegate.Add(StartBtn.onClick, () =>
+		{
+			print("StartButton is OK.");
+			//LoadScene("SCENE_LOBBY");
+		}
+			);
+
+		EventDelegate.Add(ExitBtn.onClick, () =>
+		{
+			print("ExitButton is OK.");
+			//return;
+		}
+			);
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
